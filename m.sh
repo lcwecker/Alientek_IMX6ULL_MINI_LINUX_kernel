db32b646b5b6889f23524113c54e86af5f5cf86b
lcwecker <werkerrrrr@gmail.com>
2025-12-14 01:29:05 +0800

    [INIT] initial NXP kernel, add alientek nand board

---
+++ m.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function init_clangd()
+{
+    rm -rf .cache
+    rm -f compile_commands.json
+    make V=1 -n | compiledb -v
+}
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+make distclean
+make imx_alientek_nand_defconfig
+# init_clangd
+make -j11
